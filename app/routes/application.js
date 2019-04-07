d8cada8e9bc422d71d2a14ce3d1f9f5f1174d5a8
Charles Fries <charliefries@icloud.com>
2019-04-07 15:41:51 -0700

    Installed Reddit API

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,27 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import snoowrap from 'snoowrap';
 
 export default Route.extend({
+	reddit: service(),
+	
+	model() {
+		// let res = this.reddit.api.getSubmission('2np694').body
+		// 	.then((one) => {
+		// 		console.log('Then')
+		// 		console.log(res);
+		// 	})
+		// 	.catch((one) => {
+		// 		console.log('Catch')
+		// 		console.log(res);
+		// 	});
+		
+		let res = this.reddit.api.getHot()
+			.then((data) => {
+				console.log(data);
+				return data;
+			});
+		
+		return res;
+	}
 });
---
+++ app/services/reddit.js
@@ -0,0 +1,23 @@
+import Service from '@ember/service';
+import snoowrap from 'snoowrap';
+
+export default Service.extend({
+	
+	init() {
+		this._super(...arguments);
+		// const r = new snoowrap({
+		// 	userAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
+		// 	clientId: 'BTI2fSpBFOOYVA',
+		// 	clientSecret: 'yjF2HwFxVxU2O767oJ-0XZky6Pw',
+		// 	refreshToken: 'put your refresh token here'
+		// });
+		const api = new snoowrap({
+			userAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
+			clientId: '2WbQ7U0xP1U14Q',
+			clientSecret: 'XTB55Mpq8b93mMIkkw3VKnt5AoY',
+		  username: 'charlesfries',
+		  password: 'Foxtrot7944!'
+		});
+		this.set('api', api);
+	}
+});
