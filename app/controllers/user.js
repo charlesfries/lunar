35ad437692bb6b6fcd64b7eb3e89aeaaba35a939
Charles Fries <charliefries@icloud.com>
2019-04-08 20:46:55 -0700

    Changes

---
+++ app/controllers/user.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+	actions: {
+		goToPost(name, id) {
+			this.transitionToRoute('subreddit.post', name, id);
+		}
+	}
+});
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,9 +1,24 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default Route.extend({
+	reddit: service(),
 	model({ username }) {
-		return {
-			username: username
-		};
+		console.log(username);
+		
+		let user = this.reddit.api.getUser(username);
+		
+		let submissions = this.reddit.api.getUser(username).getSubmissions()
+			.then(res => {
+				console.log(res);
+				return res;
+			});
+		return submissions;
+		
+		return hash({
+			user: user,
+			submissions: submissions
+		});
 	}
 });
