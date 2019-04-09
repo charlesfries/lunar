8f15228b8a76264d01d94f4ea8c43158ccb9ea9c
Charles Fries <charliefries@icloud.com>
2019-04-09 00:37:16 -0700

    Changes

--- app/components/post.js
+++ app/components/post.js
@@ -1,5 +1,11 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-	classNames: ['post']
+	classNames: ['post'],
+	classNameBindings: ['isActive:active'],
+	
+	isActive: computed('model.id', 'active', function() {
+		return this.model.id === this.active;
+	})
 });
--- app/routes/feed.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-	model({ name }) {
-		let controller = this.controllerFor('application');
-		controller.set('currentFeed', name);
-	}
-});
