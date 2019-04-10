bdc3133f13d2ad1b19f49a9cc0dc13bc0e436489
Charles Fries <charliefries@icloud.com>
2019-04-09 23:28:05 -0700

    Possible new navbar

--- app/components/comment.js
+++ app/components/comment.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+	classNames: ['comment'],
 	actions: {
 		toggleCollapsed() {
 			this.toggleProperty('model.collapsed');
