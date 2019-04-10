4b185718999167c1942a42794152dd7d28994a99
Charles Fries <charliefries@icloud.com>
2019-04-10 00:37:04 -0700

    Changes

--- app/components/post.js
+++ app/components/post.js
@@ -7,5 +7,11 @@ export default Component.extend({
 	
 	isActive: computed('model.id', 'active', function() {
 		return this.model.id === this.active;
-	})
+	}),
+	
+	click() {
+		let { subreddit_name_prefixed, id } = this.model;
+		subreddit_name_prefixed = subreddit_name_prefixed.replace('r/', '');
+		this.onSelection(subreddit_name_prefixed, id);
+	}
 });
