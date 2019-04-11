fc86e98c9d53a8e4dcadac96a8f2f54d88f875f8
Charles Fries <charliefries@icloud.com>
2019-04-10 18:30:04 -0700

    Renamed url-helper

--- app/helpers/url.js
+++ app/helpers/url.js
@@ -1,11 +1,11 @@
 import { helper } from '@ember/component/helper';
 
-export function urlHelper([str]) {
+export function url([str]) {
 	str = str.replace('https://www.', '');
 	str = str.replace('http://www.', '');
 	str = str.replace('https://', '');
 	str = str.replace('http://', '');
-	
+
 	if (str.length > 20) {
 		return str.substring(0, 20) + '...';
 	} else {
@@ -13,4 +13,4 @@ export function urlHelper([str]) {
 	}
 }
 
-export default helper(urlHelper);
+export default helper(url);
