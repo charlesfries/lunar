5d21adbd832dead338899d0ac59351be70bc47e7
Charles Fries <charliefries@icloud.com>
2019-04-10 20:37:20 -0700

    Changes

--- app/controllers/subreddit.js
+++ app/controllers/subreddit.js
@@ -1,7 +1,7 @@
-import Controller from '@ember/controller';
+import Controller, { inject } from '@ember/controller';
 
 export default Controller.extend({
-	currentPost: null,
+	applicationController: inject('application'),
 	actions: {
 		goToPost(name, id) {
 			this.set('currentPost', id);
