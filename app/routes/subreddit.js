c9046f0721550bb22634cb005a130f5ccb99ae4f
Charles Fries <charliefries@icloud.com>
2019-04-10 21:32:30 -0700

    Connected subreddit info fetch

--- app/routes/subreddit.js
+++ app/routes/subreddit.js
@@ -3,11 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend({
 	reddit: service(),
+	
 	model({ name }) {
+		this.set('name', name);
 		let controller = this.controllerFor('application');
 		controller.set('currentSubreddit', name);
-		
-		// return this.reddit.api.getSubreddit(name).fetch();
 		
 		return this.reddit.api.getSubreddit(name).getHot()
 			.then(posts => {
@@ -15,6 +15,15 @@ export default Route.extend({
 					post.author = Object.assign({}, post.author);
 					return post;
 				});
+			});
+	},
+	
+	setupController(controller, model) {
+		controller.set('model', model);
+		
+		this.reddit.api.getSubreddit(this.name).fetch()
+			.then(data => {
+				controller.set('subredditInfo', data);
 			});
 	}
 });
